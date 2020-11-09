59b37d78923135b32d9a29074a339f2a1fa55039
ron2618 <45929774+ron2618@users.noreply.github.com>
2020-11-10 06:59:06 +0700

    Add files via upload

---
+++ lib/router.py
@@ -0,0 +1,45 @@
+import lib.model as model
+
+class Router:
+    @staticmethod
+    def run(app):
+        
+        @app.route('/')
+        def root():
+            return model.getRootData()
+
+        @app.route('/daftar-komik')
+        def daftar_komik():
+            return model.getDaftarKomik()
+        
+        @app.route('/project-list')
+        def project_list():
+            return model.getProjectList()
+        
+        @app.route('/komik-tamat')
+        def komik_tamat():
+            return model.getKomikTamat()
+        
+        @app.route('/jadwal-update')
+        def jadwal_update():
+            return model.getJadwalUpdate()
+
+        @app.route('/komik')
+        def komik():
+            return model.getDataKomik()
+        
+        @app.route('/chapter')
+        def chapter():
+            return model.getChapterComic()
+			
+        @app.route('/genre')
+        def genre():
+            return model.getGenre()
+			
+        @app.route('/search')
+        def search():
+            return model.getSpecificComic()
+			
+		
+            
+
